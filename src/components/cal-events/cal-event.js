1e7804331dcbc55529d810920b81f6050f66b03e
khatauli <sachin.kumar.v@gmail.com>
2017-04-27 23:13:55 -0700

    cleaned out un-necessary divs and table.

---
+++ src/components/cal-events/cal-event.js
@@ -0,0 +1,36 @@
+Polymer({
+  is: 'cal-event',
+  
+  properties: {
+    id: {
+      type: String,
+    },
+    name: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    type: {
+      type: String,
+    },
+    venue: {
+      type: String,
+    },        
+    location: {
+      type: String,
+    },
+    time: {
+      type: String,
+    },
+    duration: {
+      type: String,
+    },
+    attendees: {
+      type: String,
+    },
+  },   
+});
