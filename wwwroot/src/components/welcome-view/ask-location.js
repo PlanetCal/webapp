8ff3eab4b979a3b986e872fcdc9094f7c324b5eb
khatauli <sachin.kumar.v@gmail.com>
2017-05-28 08:51:36 -0700

    Added a new country region element

--- wwwroot/src/components/welcome-view/ask-location.js
+++ wwwroot/src/components/welcome-view/ask-location.js
@@ -2,10 +2,13 @@ Polymer({
   is: 'ask-location',
   listeners: {
     '--cal-stepper-next-page-requested': 'forwardhandler',
-    '--cal-stepper-previous-page-requested': 'previoushandler'
+    '--cal-stepper-previous-page-requested': 'previoushandler',
+    '--on-country-changed': 'countryChangedHandler',
+    '--on-region-changed': 'regionChangedHandler'
   },
 
   ready: function () {
+    this.countryValue = "US";
     this.product = Polymer.globalsManager.globals.product.displayName;
     var loggedInUser = Polymer.globalsManager.globals.loggedInUser;
     if (loggedInUser) {
@@ -22,5 +25,13 @@ Polymer({
 
   forwardhandler: function () {
     this.fire('page-load-requested', { page: '/about' });
+  },
+
+  countryChangedHandler: function (e) {
+    this.countryValue = e.detail.countryValue;
+  },
+
+  regionChangedHandler: function (e) {
+    this.regionValue = e.detail.regionValue;
   },
 });
