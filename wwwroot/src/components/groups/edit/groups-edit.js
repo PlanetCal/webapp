dcf897eb21785d83487f8ef36d82364ef547cb9a
v.rajarajan@gmail.com <v.rajarajan@gmail.com>
2017-07-25 01:06:46 -0700

    Adding Ajax calls and few more validations around image upload.

--- wwwroot/src/components/groups/edit/groups-edit.js
+++ wwwroot/src/components/groups/edit/groups-edit.js
@@ -12,10 +12,18 @@ Polymer({
             value: false,
             notify: true,
         },
-        isImageChanged: {
+        isGroupImageChanged: {
             type: Boolean,
             value: false,
             notify: true,
+        },
+        groupType: {
+            type: String,
+            value: '',
+        },
+        groupObject: {
+            type: Object,
+            value: {},
         },
     },
     ready: function () {
@@ -28,8 +36,8 @@ Polymer({
         } else {
             this.reset();
         }
-        var groupDetails = Polymer.globalsManager.globals.groupDetails;
-        var dd = groupDetails;
+        // var groupDetails = Polymer.globalsManager.globals.groupDetails;
+        // var dd = groupDetails;
     },
 
     // validateOnChange: function () {
@@ -51,16 +59,30 @@ Polymer({
         }
     },
     previewImage: function (e) {
-        var output = e.target.inputElement.files[0];
-        //document.getElementById('PreviewImage').src = window.URL.createObjectURL(file);
-        this.displayContents(output);
+        var inputElement = e.target.inputElement;
+        if (inputElement.files && inputElement.files.length > 0) {
+            if (this.isValidFileType(inputElement.files[0].type)) {
+                var output = inputElement.files[0];
+                //document.getElementById('PreviewImage').src = window.URL.createObjectURL(file);
+                this.displayContents(output);
+                this.isGroupImageChanged = true;
+            }
+            else {
+                inputElement.value = '';
+                this.fire("status-message-update", { severity: 'error', message: 'Invalid file type selected. Please select an image to upload.' });
+            }
+        }
+        else if (this.isGroupImageChanged) {
+            this.previewSrc = '';
+        }
     },
     displayContents: function (output) {
         //this.$.PreviewImage.src = window.URL.createObjectURL(output);
         this.previewSrc = window.URL.createObjectURL(output);
     },
-    resizeImageSelection: function (e) {
-        var file = e.target.inputElement.files[0];
+    resizeImageSelection: function () {
+        //var file = e.target.inputElement.files[0];
+        var file = this.$.groupImage.inputElement.files[0];
         var img = document.createElement("img");
         img.src = window.URL.createObjectURL(file);
 
@@ -171,17 +193,151 @@ Polymer({
         return isValid;
         //document.getElementsByClassName('.addEvent').validate();
     },
+    validateGroupImage: function () {
+        if (!this.previewSrc || this.previewSrc == '')
+            return this.$$('#groupImage').validate();
+        else
+            return true;
+    },
     validateOnChange: function (e) {
-        e.target.validate();
+        e.target.$.input.validate();
+    },
+    isValidFileType: function (fileType) {
+        if (fileType == 'image/jpeg' || fileType == 'image/bmp' || fileType == 'image/png') {
+            return true;
+        }
+        else {
+            return false;
+        }
+    },
+    constructGroupObject: function () {
+        var obj = {};
+        if (this.id && this.id !== 'null') {
+            obj.id = this.id;
+        }
+
+        obj.name = this.name;
+        obj.description = this.description;
+        obj.address = {};
+        obj.address.streetNumber = this.streetNumber;
+        obj.address.streetName = this.streetName;
+        obj.address.city = this.city;
+        obj.address.state = this.state;
+        obj.address.postalCode = this.postalCode;
+        obj.location = this.location;
+        obj.contact = {};
+        obj.contact.phone = this.phone;
+        obj.contact.email = this.email;
+        obj.owner = this.owner;
+        obj.webSite = this.website;
+        obj.administrators = this.administrators;
+        obj.members = this.members;
+        obj.privacy = this.privacy;
+        //obj.icon = this.previewSrc;
+        return obj;
     },
     saveGroup: function (e) {
         var isValid = this.validate();
+        isValid = isValid && this.validateGroupImage();
+        isValid = isValid && this.isGroupImageChanged && this.isValidFileType(this.$.groupImage.inputElement.files[0]);
         if (isValid) {
             this.$.savegroup.disabled = true;
             //this.$.cancelevent.disabled = true;
-            //this.eventObject = this.constructEventObject();
-            //this.eventType = this.eventObject.id ? 'putEvents' : 'postEvents';
-            //this.makeAjaxCall(this.eventObject);
+            this.groupObject = this.constructGroupObject();
+            this.groupType = this.groupObject.id ? 'putgroup' : 'postgroup';
+            //this.makeAjaxCall(this.groupObject);
+            this.resizeImageSelection();
         }
     },
+
+    makeAjaxCall: function (group = null) {
+        //this.isLoading = true;
+        var ajax = this.$.ajax;
+        var serviceBaseUrl = Polymer.globalsManager.globals.serviceBaseUrl;
+        var loggedInUser = Polymer.globalsManager.globals.loggedInUser;
+        ajax.url = serviceBaseUrl + '/groups/';
+        switch (this.groupType) {
+            case 'postgroup':
+                ajax.body = JSON.stringify(group);
+                ajax.method = 'POST';
+                ajax.headers['Version'] = '1.0';
+                if (loggedInUser) {
+                    ajax.headers['Authorization'] = 'Bearer ' + loggedInUser.token;
+                }
+                break;
+            case 'putgroup':
+                ajax.url += group.id;
+                ajax.body = JSON.stringify(group);
+                ajax.method = 'PUT';
+                ajax.headers['Version'] = '1.0';
+                if (loggedInUser) {
+                    ajax.headers['Authorization'] = 'Bearer ' + loggedInUser.token;
+                }
+                break;
+            case 'deleteGroup':
+                ajax.url += group.id;
+                // ajax.body = JSON.stringify(event.id);
+                ajax.method = 'DELETE';
+                ajax.headers['Version'] = '1.0';
+                if (loggedInUser) {
+                    ajax.headers['Authorization'] = 'Bearer ' + loggedInUser.token;
+                }
+                break;
+            case 'groupImage':
+                ajax.url = serviceBaseUrl + '/blob/' + this.groupObject.id;
+                ajax.body = group;
+                ajax.method = 'POST';
+                ajax.headers['Version'] = '1.0';
+                if (loggedInUser) {
+                    ajax.headers['Authorization'] = 'Bearer ' + loggedInUser.token;
+                }
+                ajax.headers['groupid'] = this.groupObject.id;
+                break;
+        }
+        ajax.generateRequest();
+    },
+
+    handleErrorResponse: function (e) {
+        console.log('Save group got error from ajax!');
+        this.fire("status-message-update", { severity: 'error', message: 'Error occurred on save groups...' });
+        var req = e.detail.request;
+        var jsonResponse = e.detail.request.xhr.response;
+        var message = 'Error:';
+        message = message + ' Here are the Details: Error Status: ' + req.status + ' Error StatusText: ' + req.statusText;
+    },
+
+    handleAjaxResponse: function () {
+        switch (this.groupType) {
+            case 'postgroup':
+                this.groupObject.id = event.detail.response.id;
+                this.uploadImage();
+                //this.fire("status-message-update", { severity: 'info', message: 'Event saved successfully ...' });
+                break;
+            case 'putEvents':
+                if (this.isGroupImageChanged) {
+                    this.uploadImage();
+                    this.isGroupImageChanged = false;
+                }
+                else {
+                    this.backToGroups();
+                }
+                break;
+            case 'deleteEvents':
+                //TODO: do we need to delete the image?
+                this.backToGroups();
+                break;
+            case 'groupImage':
+                this.updateImageURL(event.detail.response.id);
+                break;
+
+        }
+    },
+    uploadImage: function () {
+        this.groupType = 'groupImage';
+        this.makeAjaxCall(this.resizeImageSelection());
+    },
+    updateImageURL: function (imageURL) {
+        this.groupObject.icon = imageURL;
+        this.makeAjaxCall(this.groupObject);
+    }
 });
